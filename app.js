fdcb55099a4279b5e2daf13cd01d08bda6868709
Erein0v0 <erein0v0@gmail.com>
2026-05-25 19:14:51 +0800

    提交文件

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 App({
   onLaunch() {
-    wx.cloud.init({ env: 'cloud1-0g9ypxoqbc4fb323', traceUser: true })
+    wx.cloud.init({ env: 'YOUR_ENV_ID', traceUser: true })// ★ 填入真实云开发环境ID
   },
   globalData: {
     // ★ 修改为真实宿舍成员姓名
-    roommates: ['唐诚俊', '黄炜廉', '张世奕', '郭银海'],
+    roommates: ['张三', '李四', '王五', '赵六'],
   },
 })
